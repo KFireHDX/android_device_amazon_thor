5d6bd7b1fef4113b17c740c8849d1787c4fd121e
Cpasjuste <cpasjuste@sd-70635.dedibox.fr>
2014-12-17 17:29:45 +0100

    first commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,18 +5,9 @@
 -include vendor/amazon/hdx-common/BoardConfigVendor.mk
 
 # Kernel config
-#TODO: add prebuild kernel 
-#TARGET_PREBUILT_KERNEL := device/amazon/thor/kernel
-# compatibility sources from msm project (codeaurora) has proper amz kernel sources are not released
-#TARGET_KERNEL_SOURCE := kernel/qcom/AU_LINUX_ANDROID_LNX.LA.3.5.1.2.04.04.02.052.004
-#TARGET_KERNEL_CONFIG := msm8974-perf_defconfig
-#TARGET_KERNEL_SOURCE := kernel/amazon/hdx-common
-#TARGET_KERNEL_CONFIG := thor-cm_defconfig 
-#BOARD_MKBOOTIMG_ARGS += --dt device/amazon/thor/thor-v2-apq.dtb
-
-TARGET_PREBUILT_KERNEL := device/amazon/thor/hacks/zImage
-TARGET_PREBUILT_RECOVERY_KERNEL := device/amazon/thor/hacks/zImage
-
+TARGET_PREBUILT_KERNEL := device/amazon/thor/hacks/zImage-3.2.4
+TARGET_PREBUILT_RECOVERY_KERNEL := device/amazon/thor/hacks/zImage-3.2.4
+BOARD_MKBOOTIMG_ARGS += --dt device/amazon/thor/hacks/dt-3.2.4.img
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := thor
